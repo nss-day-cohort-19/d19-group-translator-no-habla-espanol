375faf8da857e62ae1d0c7248ae4a93c342a7f99
Chaz Henricks <chazhenricks@gmail.com>
2017-04-25 15:57:38 -0500

    started adding functionality

--- js/translator.js
+++ js/translator.js
@@ -1,4 +1,48 @@
+
+
 var Translator = (function (){
+    var toTranslate;
+
+    return {
+        setFrench: function(text){
+            toTranslate = text;
+        },
+        getFrench: function(){
+            return toTranslate;
+        }
+    }
 
 
 })();
+
+
+var translateBtn = document.getElementById("btn-translate");
+translateBtn.addEventListener("click", function(event){
+
+
+    //Store user text into variable
+
+    var userText = document.getElementById("user-text").value;
+
+    //Get user translate option
+    var language;
+    var translateOption = document.getElementsByName("translate-language");
+    for(var i=0;i<translateOption.length;i++){
+        if(translateOption[i].value === "French"){
+                Translator.setFrench(userText);
+        }
+    }
+
+
+
+    console.log("toTranslate", Translator.getFrench());
+
+    //pass user input into desired translate option
+
+    //return translation
+
+    //add to div on page
+
+
+
+})
